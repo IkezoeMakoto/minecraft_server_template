6bbe24abb22443a8d70a9f1dcb4a685266f2f3ba
IkezoeMakoto <150283t@gmail.com>
2021-04-11 17:07:38 +0900

    backup script書いた

---
+++ scripts/full-backup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -eu
+# debug mode
+debug=${debug:-}
+# shellcheck disable=SC2046
+cd `dirname $0`
+# shellcheck disable=SC1090
+. ../envs/$ENV
+
+MC_PATH=../servers/$SERVER_DIR
+BK_GEN="3"
+
+BK_DIR=../backups
+BK_TIME=`date +%Y%m%d-%H%M%S`
+BK_PREFIX="mc_full_"
+BK_SUFFIX=".tar.gz"
+BK_NAME=$BK_PREFIX$BK_TIME$BK_SUFFIX
+BK_PATH="$BK_DIR/$ENV/$BK_NAME"
+
+echo "[`date`]: ==== Full backup start mc-server@$ENV data ===="
+
+# サービスが生きてるか
+if $debug systemctl status mc-server@$ENV > /dev/null
+  then
+    echo "[`date`]: > Stopping mc-server@$ENV ..."
+    # サービスを一旦停止
+    $debug systemctl stop mc-server@$ENV
+    # 停止まで30sかかるので待つ
+    $debug sleep 32
+    echo "[`date`]: > Stopped mc-server@$ENV"
+
+    echo "[`date`]: >> Full Backup start ..."
+    # バックアップ
+    $debug tar cfz $BK_PATH $MC_PATH
+    # 完了するまで念の為ちょっと待つ
+    $debug sleep 10
+    echo "[`date`]: >> Full Backup compleate!"
+
+    echo "[`date`]: >> Old Backup removing ..."
+    # 古いものを削除
+    $debug find $BK_DIR/$ENV -name "$BK_PREFIX*$BK_SUFFIX" -type f -mtime +$BK_GEN -exec rm {} \;
+    echo "[`date`]: >> Old Backup remove done!"
+
+    # 止めてたサービスを開始する
+    echo "[`date`]: > Starting mc-server@$ENV ..."
+    $debug systemctl start mc-server@$ENV
+    echo "[`date`]: Full backup end!"
+  else
+    echo "[`date`]: mc-server@$ENV was not runnning."
+fi
