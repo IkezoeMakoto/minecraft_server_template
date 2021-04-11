5d4cc15527659cc67de78a1a63a6f4fa43b9d6b6
IkezoeMakoto <150283t@gmail.com>
2021-04-11 14:24:11 +0900

    serversへの変更漏れ

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -eu
 # shellcheck disable=SC2046
 cd `dirname $0`
-cd ../server
-
-java -Xmx2048M -Xms2048M -jar ./${SERVER:-minecraft.jar} nogui
+# SERVERからパスを取得して移動
+cd ../servers/${SERVER%/*}
+# SERVERからファイルを取得して実行
+java -Xmx2048M -Xms2048M -jar ./${SERVER##*/} nogui
